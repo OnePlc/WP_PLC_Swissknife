773149d878acf22b3359497e459055d9f760fea1
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2019-12-04 18:06:11 +0100

    Version bump

--- wpplc-swissknife/wpplc-swissknife.php
+++ wpplc-swissknife/wpplc-swissknife.php
@@ -11,7 +11,7 @@
  * Plugin Name: WP PLC Swissknife
  * Plugin URI:  https://1plc.ch/wordpress-plugins/swissknife
  * Description: onePlace Swissknife for Wordpress. Increase Wordpress Security and Performance
- * Version:     0.3-stable
+ * Version:     0.3.2-stable
  * Author:      Verein onePlace
  * Author URI:  https://1plc.ch
  * License:     GNU General Public License, version 2
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define Version and directories for further use in plugin
-define( 'WPPLC_SWISSKNIFE_VERSION', '0.3-stable' );
+define( 'WPPLC_SWISSKNIFE_VERSION', '0.3.2-stable' );
 define( 'WPPLC_SWISSKNIFE_MAIN_FILE', __FILE__ );
 define( 'WPPLC_SWISSKNIFE_MAIN_DIR', __DIR__ );
 define( 'WPPLC_SWISSKNIFE_PUB_DIR',str_replace([$_SERVER['DOCUMENT_ROOT']],[''],WPPLC_SWISSKNIFE_MAIN_DIR));
