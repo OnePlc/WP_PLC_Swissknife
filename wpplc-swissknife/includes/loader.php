f32372ffe0358dd45d17e4c25bd9ea2b457bfe59
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2019-12-04 18:29:25 +0100

    remove error display

--- wpplc-swissknife/includes/loader.php
+++ wpplc-swissknife/includes/loader.php
@@ -28,9 +28,5 @@ require_once __DIR__.'/Modules/Revisions.php';
 require_once __DIR__.'/Modules/Sitekit.php';
 require_once __DIR__.'/Modules/Tweaks.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 Plugin::load(WPPLC_SWISSKNIFE_MAIN_FILE);
 
